453809684840c3a58eb3aa058e29feab9d721bf6
nandhayoyo <nandha.yuana2@Gmail.com>
2023-10-17 17:02:24 +0700

    feat: hero section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Navbar from "./component/Navbar";
+import Hero from "./component/Hero";
 
 function App() {
   return (
     <>
-        <Navbar />
+      <Navbar />
+      <Hero />
     </>
   );
 }
