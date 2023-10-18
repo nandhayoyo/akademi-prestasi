8ce968d88bcb13fc9a30c28d0a98655879e1d204
nandhayoyo <nandha.yuana2@Gmail.com>
2023-10-18 14:57:59 +0700

    config theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {},
+  daisyui: {
+    themes: ["light"],
   },
+
   plugins: [require("daisyui")],
   fontFamily: {
     body: ["Montserrat"],
