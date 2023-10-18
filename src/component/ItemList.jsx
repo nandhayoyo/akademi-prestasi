291bd9f36cfbec1367a173b9d4bfda77515a6863
nandhayoyo <nandha.yuana2@Gmail.com>
2023-10-18 15:23:05 +0700

    try fix seo

--- src/component/ItemList.jsx
+++ src/component/ItemList.jsx
@@ -22,7 +22,7 @@ const ItemList = () => {
       console.error("Terjadi kesalahan saat mengambil data:", error);
     }
   };
-//
+  //
   useEffect(() => {
     fetchData();
   }, [limit]);
@@ -73,9 +73,7 @@ const ItemList = () => {
                 >
                   <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
                 </svg>
-                <p className="ml-1 text-sm font-bold">
-                  {item.rating}
-                </p>
+                <p className="ml-1 text-sm font-bold">{item.rating}</p>
               </div>
               <div className="flex items-center mt-2">
                 <img src={map_img} width={20} className="mr-1" />
